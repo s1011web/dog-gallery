142cb9a17c755a6908509a9873dbd5351e41dffe
s1011web <sumikawa.takumu@gmail.com>
2021-04-05 22:53:53 +0900

    make dog-gallery by dog api

---
+++ src/api.js
@@ -0,0 +1,7 @@
+export async function fetchImages(breed) {
+  const res = await fetch(
+    `https://dog.ceo/api/breed/${breed}/images/random/12`
+  );
+  const data = await res.json();
+  return data.message;
+}
